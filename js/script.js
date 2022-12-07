351730a0dcb551d8938ff20a44f87a00fdbc3d0d
Sebastian-ivaniczki <sivaniczki@gmail.com>
2022-12-07 14:48:41 +0100

    acted function for multiples of 5

--- js/script.js
+++ js/script.js
@@ -21,6 +21,9 @@ for(let i = 1; i <= 100; i++){
     
     if(i % 3 === 0){
         number = 'FIZZ';
+    }else if(i % 5 === 0){ 
+        number = 'BUZZ'
+    
     }else{
         number = i
     }
